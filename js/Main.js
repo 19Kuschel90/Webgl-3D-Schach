cf7d7025d1219fbab7bf7cb18aa1264ab4acce5e
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-10 15:50:06 +0200

    C_ShaderBuilder bug argument

--- js/Main.js
+++ js/Main.js
@@ -37,17 +37,18 @@ function main(vertex_shader, fragment_shader) {
     gRLoop = new C_RenderLoop(onRender, 30);
     //....................................
     //Load up resources
-    Resources.setup(gl, onReady).loadTexture("atlas", "../image/atlas_mindcraft.png").start();
+    C_Resources.setup(gl, onReady).loadTexture("atlas", "../image/atlas_mindcraft.png").start();
 }
 //==================================================
 //When Main System is setup and all resources are downloaded.
 function onReady() {
     //Setup Test Shader, Modal, Meshes
-    gShader = new C_ShaderBuilder(gl, gvertex_shader, gfragment_shader);
-    // .prepareUniforms(["uPMatrix","uMVMatrix","uCameraMatrix","uFaces"],["mat4","mat4","mat4","2fv" ])
-    // .prepareTextures("uAltas","atlas")
+    gShader = new C_ShaderBuilder(gl, gvertex_shader, gfragment_shader)
+        .mapFix(["uPMatrix", "uMVMatrix", "uCameraMatrix", "uFaces"], ["mat4", "mat4", "mat4", "2fv"], ["uAltas"], ["atlas"], "uPMatrix", gCamera.projectionMatrix);
+    //	 .prepareUniforms(["uPMatrix","uMVMatrix","uCameraMatrix","uFaces"],["mat4","mat4","mat4","2fv" ])
+    //   .prepareTextures(["uAltas"],["atlas"])
     // .setUniforms("uPMatrix",gCamera.projectionMatrix);
-    //,"uFaces", [8,0, 8,0, 8,0, 10,0, 8,0, 9,0] ); //Front,Back,Left,Bottom,Right,Top
+    // 			,"uFaces", [8,0, 8,0, 8,0, 10,0, 8,0, 9,0] ); //Front,Back,Left,Bottom,Right,Top
     //gModel = Primatives.Cube.createModal(gl,"Cube",true)
     //		.setPosition(0,0.6,0);//.setScale(0.7,0.7,0.7);
     var cubemesh = Primatives.Cube.createMesh(gl, "Cube", 1, 1, 1, 0, 0, 0, false);
@@ -70,7 +71,7 @@ function onRender(dt) {
     gShader.preRender("uCameraMatrix", gCamera.viewMatrix);
     //.renderModel(gModel.preRender(),false);
     for (var i = 0; i < gCubes.length; i++) {
-        gShader.setUniforms("uFaces", texMap[i]).renderModel(gCubes[i].preRender());
+        gShader.setUniformsFix("uFaces", texMap[i]).renderModel(gCubes[i].preRender());
     }
 }
 //# sourceMappingURL=Main.js.map
