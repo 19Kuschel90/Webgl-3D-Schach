7f8fa80082b3859ce97b236d7e21860b849c898d
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-10 15:51:10 +0200

    No button

--- js/Main.ts
+++ js/Main.ts
@@ -42,7 +42,7 @@ function main(vertex_shader:string,fragment_shader:string):void
 
 				//....................................
 				//Load up resources
-				Resources.setup(gl,onReady).loadTexture("atlas","../image/atlas_mindcraft.png").start();
+				C_Resources.setup(gl,onReady).loadTexture("atlas","../image/atlas_mindcraft.png").start();
 }
 
 	//==================================================
@@ -50,10 +50,12 @@ function main(vertex_shader:string,fragment_shader:string):void
 			function onReady(){
 				//Setup Test Shader, Modal, Meshes
 				gShader = new C_ShaderBuilder(gl,gvertex_shader,gfragment_shader)
-					// .prepareUniforms(["uPMatrix","uMVMatrix","uCameraMatrix","uFaces"],["mat4","mat4","mat4","2fv" ])
-					// .prepareTextures("uAltas","atlas")
+				.mapFix(["uPMatrix","uMVMatrix","uCameraMatrix","uFaces"],["mat4","mat4","mat4","2fv" ],
+				["uAltas"],["atlas"],"uPMatrix",gCamera.projectionMatrix);
+			//	 .prepareUniforms(["uPMatrix","uMVMatrix","uCameraMatrix","uFaces"],["mat4","mat4","mat4","2fv" ])
+				//   .prepareTextures(["uAltas"],["atlas"])
 					// .setUniforms("uPMatrix",gCamera.projectionMatrix);
-								//,"uFaces", [8,0, 8,0, 8,0, 10,0, 8,0, 9,0] ); //Front,Back,Left,Bottom,Right,Top
+					// 			,"uFaces", [8,0, 8,0, 8,0, 10,0, 8,0, 9,0] ); //Front,Back,Left,Bottom,Right,Top
 
 				//gModel = Primatives.Cube.createModal(gl,"Cube",true)
 				//		.setPosition(0,0.6,0);//.setScale(0.7,0.7,0.7);
@@ -84,7 +86,7 @@ function onRender(dt:any):void{
 					//.renderModel(gModel.preRender(),false);
 
 				for(var i=0; i < gCubes.length; i++){
-					gShader.setUniforms("uFaces",texMap[i]).renderModel( gCubes[i].preRender() );
+					gShader.setUniformsFix("uFaces",texMap[i]).renderModel( gCubes[i].preRender() );
 				}
 
 }
