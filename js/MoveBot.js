de28519747461a3572f321d52df150266d5c33eb
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-15 14:18:40 +0200

    udate ver bessert

--- js/MoveBot.js
+++ js/MoveBot.js
@@ -1,14 +1,58 @@
 "use strict";
 var C_MoveBot = /** @class */ (function () {
-    function C_MoveBot(ObjectTransform, TragetVec, imRun) {
+    function C_MoveBot(ObjectTransform, TragetVec, imRun, Speed) {
         if (ObjectTransform === void 0) { ObjectTransform = new C_Transform(); }
         if (TragetVec === void 0) { TragetVec = new C_Vector3(0, 0, 0); }
         if (imRun === void 0) { imRun = false; }
-        this.TragetVec = new C_Vector3(0, 0, 0);
+        if (Speed === void 0) { Speed = 1; }
+        this.speed = 1;
         this.MyObjectTransform = ObjectTransform;
+        this.TragetVec = TragetVec;
         this.isRun = imRun;
+        this.speed = Speed;
     }
     C_MoveBot.prototype.Update = function () {
+        console.log(this.MyObjectTransform.position.x);
+        console.log(this.TragetVec.x);
+        if (this.MyObjectTransform.position.x === this.TragetVec.x) {
+        }
+        else {
+            if (this.MyObjectTransform.position.x < this.TragetVec.x) {
+                this.MoveX(1);
+            }
+            else {
+                this.MoveX(-1);
+            }
+        }
+        if (this.MyObjectTransform.position.z === this.TragetVec.z) {
+        }
+        else {
+            if (this.MyObjectTransform.position.z < this.TragetVec.z) {
+                this.MoveZ(1);
+            }
+            else {
+                this.MoveZ(-1);
+            }
+        }
+    };
+    // step * speed
+    C_MoveBot.prototype.MoveZ = function (step) {
+        this.MyObjectTransform.position.z += step * this.speed;
+    };
+    // step * speed
+    C_MoveBot.prototype.MoveX = function (step) {
+        this.MyObjectTransform.position.x += step * this.speed;
+    };
+    // step * speed
+    C_MoveBot.prototype.MoveY = function (step) {
+        this.MyObjectTransform.position.y += step * this.speed;
+    };
+    ////////////////////////////////////////////
+    C_MoveBot.prototype.GetSpeed = function () {
+        return this.speed;
+    };
+    C_MoveBot.prototype.SetSpeed = function (Speed) {
+        this.speed = Speed;
     };
     //////////////////////////////////////////////////////
     C_MoveBot.prototype.GetMyObjectTransform = function () {
--- js/MoveBot.ts
+++ js/MoveBot.ts
@@ -1,22 +1,74 @@
 
 class C_MoveBot{
+    private MyObjectTransform:C_Transform;
    private TragetVec:C_Vector3;
-   private MyObjectTransform:C_Transform;
    private isRun:boolean;
+   private speed:number = 1;
     constructor( 
         ObjectTransform:C_Transform = new C_Transform()
         ,TragetVec:C_Vector3 = new C_Vector3(0,0,0)      
         ,imRun:boolean = false
-) {
-        this.TragetVec = new C_Vector3(0,0,0);
+        ,Speed:number = 1
+    ) {
         this.MyObjectTransform = ObjectTransform;
+        this.TragetVec = TragetVec;
         this.isRun = imRun;
+        this.speed = Speed;
     }
 
 
     Update():void{
+        console.log(this.MyObjectTransform.position.x);
+        console.log(this.TragetVec.x);
+        if(this.MyObjectTransform.position.x === this.TragetVec.x)
+        {
+            
+        }else{
+            if(this.MyObjectTransform.position.x < this.TragetVec.x)
+            {
+                this.MoveX(1);               
+            }else{
+                 this.MoveX(-1);
+            }
+        }
+        if(this.MyObjectTransform.position.z === this.TragetVec.z)
+        {
 
+        }else{
+            if(this.MyObjectTransform.position.z < this.TragetVec.z)
+            {
+                this.MoveZ(1);               
+            }else{
+                 this.MoveZ(-1);
+            }
+        }
     }
+
+
+    // step * speed
+    MoveZ(step:number){
+        this.MyObjectTransform.position.z += step * this.speed;
+    }
+
+    // step * speed
+    MoveX(step:number){
+        this.MyObjectTransform.position.x += step * this.speed;
+    }
+
+    // step * speed
+    MoveY(step:number){
+        this.MyObjectTransform.position.y += step * this.speed;
+    }
+ 
+    ////////////////////////////////////////////
+    GetSpeed():number{
+         return this.speed;
+    }
+
+    SetSpeed(Speed:number){
+        this.speed = Speed;
+    }
+
 //////////////////////////////////////////////////////
     GetMyObjectTransform():C_Transform{
         return this.MyObjectTransform;
