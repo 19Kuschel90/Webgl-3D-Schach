12b544f7344180a70f6a0d0e631f92c04e8af0aa
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-22 13:35:55 +0200

    New Class C_InputManager

---
+++ js/HtmlInputManager.js
@@ -0,0 +1,13 @@
+"use strict";
+var C_InputManager = /** @class */ (function () {
+    function C_InputManager() {
+        this.atlasLink = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
+        this.startPos = new C_Vector3(8, 15, -20);
+        this.canvasSizeW = 1024;
+        this.canvasSizeH = 1024;
+    }
+    C_InputManager.prototype.update = function () {
+    };
+    return C_InputManager;
+}());
+//# sourceMappingURL=HtmlInputManager.js.map
---
+++ js/HtmlInputManager.ts
@@ -0,0 +1,11 @@
+class  C_InputManager {
+  public atlasLink:string = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
+  public startPos:C_Vector3 = new C_Vector3(8,15,-20);
+  public canvasSizeW:number = 1024;
+  public canvasSizeH:number = 1024;
+    constructor() {
+    }
+    update():void{
+
+    }
+}
