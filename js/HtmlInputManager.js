ec6591adf93d8b786d012d0cf9b0a0282b1f1d23
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-24 20:33:09 +0200

    1024 to 600

--- js/HtmlInputManager.js
+++ js/HtmlInputManager.js
@@ -3,8 +3,8 @@ var C_InputManager = /** @class */ (function () {
     function C_InputManager() {
         this.atlasLink = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
         this.startPos = new C_Vector3(8, 15, -20);
-        this.canvasSizeW = 1024;
-        this.canvasSizeH = 1024;
+        this.canvasSizeW = 600;
+        this.canvasSizeH = 600;
     }
     C_InputManager.prototype.update = function () {
     };
--- js/HtmlInputManager.ts
+++ js/HtmlInputManager.ts
@@ -1,8 +1,8 @@
 class  C_InputManager {
   public atlasLink:string = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
   public startPos:C_Vector3 = new C_Vector3(8,15,-20);
-  public canvasSizeW:number = 1024;
-  public canvasSizeH:number = 1024;
+  public canvasSizeW:number = 600;
+  public canvasSizeH:number = 600;
     constructor() {
     }
     update():void{
