6a13f319151e5f4da81d9c3cebd623c973b0416e
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-25 18:51:18 +0200

    new HTMLInputElement

--- js/HtmlInputManager.js
+++ js/HtmlInputManager.js
@@ -1,12 +1,17 @@
 "use strict";
 var C_InputManager = /** @class */ (function () {
     function C_InputManager() {
-        this.atlasLink = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
+        this.atlasLink = "../image/AiuiPlay_730x730.png";
         this.startPos = new C_Vector3(8, 15, -20);
         this.canvasSizeW = 600;
         this.canvasSizeH = 600;
     }
     C_InputManager.prototype.update = function () {
+        this.startPos.x = Number(document.getElementById("SX").value);
+        this.startPos.y = Number(document.getElementById("SY").value);
+        this.startPos.z = Number(document.getElementById("SZ").value);
+        this.canvasSizeW = Number(document.getElementById("canvasSizeW").value);
+        this.canvasSizeH = Number(document.getElementById("canvasSizeH").value);
     };
     return C_InputManager;
 }());
--- js/HtmlInputManager.ts
+++ js/HtmlInputManager.ts
@@ -1,11 +1,16 @@
 class  C_InputManager {
-  public atlasLink:string = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
+  public atlasLink:string = "../image/AiuiPlay_730x730.png";
   public startPos:C_Vector3 = new C_Vector3(8,15,-20);
   public canvasSizeW:number = 600;
   public canvasSizeH:number = 600;
     constructor() {
     }
     update():void{
+      this.startPos.x = Number((<HTMLInputElement>document.getElementById("SX")).value);
+      this.startPos.y = Number((<HTMLInputElement>document.getElementById("SY")).value);
+      this.startPos.z = Number((<HTMLInputElement>document.getElementById("SZ")).value);
+      this.canvasSizeW =  Number((<HTMLInputElement>document.getElementById("canvasSizeW")).value)
+      this.canvasSizeH =  Number((<HTMLInputElement>document.getElementById("canvasSizeH")).value)
 
     }
 }
