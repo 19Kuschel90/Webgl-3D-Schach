4c00c252b6750d926e58a27f8a33f06d59b89f15
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-04-30 16:23:42 +0200

    AiuiPlay_730x730 to c62bb9b27329447cb2b937fe6213889a

--- js/HtmlInputManager.js
+++ js/HtmlInputManager.js
@@ -1,7 +1,7 @@
 "use strict";
 var C_InputManager = /** @class */ (function () {
     function C_InputManager() {
-        this.atlasLink = "../image/AiuiPlay_730x730.png";
+        this.atlasLink = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
         this.startPos = new C_Vector3(8, 15, -20);
         this.canvasSizeW = 600;
         this.canvasSizeH = 600;
--- js/HtmlInputManager.ts
+++ js/HtmlInputManager.ts
@@ -1,5 +1,5 @@
 class  C_InputManager {
-  public atlasLink:string = "../image/AiuiPlay_730x730.png";
+  public atlasLink:string = "../image/c62bb9b27329447cb2b937fe6213889a.jpg";
   public startPos:C_Vector3 = new C_Vector3(8,15,-20);
   public canvasSizeW:number = 600;
   public canvasSizeH:number = 600;
