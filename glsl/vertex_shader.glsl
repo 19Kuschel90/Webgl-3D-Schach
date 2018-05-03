48608f5625b4cfd89fdf7707750ef6b20827579c
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-05-03 17:53:52 +0200

    remove uPositonX  uPositonY size, add ublackWite

--- glsl/vertex_shader.glsl
+++ glsl/vertex_shader.glsl
@@ -6,17 +6,14 @@
 		uniform mat4 uPMatrix;
 		uniform mat4 uMVMatrix;
 		uniform mat4 uCameraMatrix;
-		uniform float uPositonX;
-		uniform float uPositonY;
+		uniform float ublackWite;
 		out highp vec2 vUV;
 
-		const float size = 1.0/16.0;
 		
 		void main(void){
 			int f = int(a_position.w);
-			float u = uPositonX * size + a_uv.x * size;
-			float v = uPositonY* size + a_uv.y * size;
-			vUV = vec2(u,v);
+
+			vUV = vec2(ublackWite,1.0f);
 
 			gl_Position = uPMatrix * uCameraMatrix * uMVMatrix * vec4(a_position.xyz, 1.0); 
 		}
