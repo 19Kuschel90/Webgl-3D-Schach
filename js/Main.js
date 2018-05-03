1b3dac131b3f29aab3091bbc87aa1e73bfe8e48f
19Kuschel90 <h.kuschel.rohrbach@gmx.de>
2018-05-03 14:08:36 +0200

    main to G_LoadShader new gShaderFunction.. initCubs initShader

--- js/Main.js
+++ js/Main.js
@@ -1,49 +1,36 @@
 "use strict";
 window.addEventListener("load", function () {
-    main();
+    G_LoadShader();
 });
 var gl = null;
 var gModal = null;
 var gCamera;
 var gCameraCtrl;
-var gShader = null;
-var gGridShader = null;
-var gGridModal = null;
 var RLoop = null;
 var gRLoop;
 var Resources = null;
 var gCubes = [];
 var gInputManager = new C_InputManager();
-var gVertex_shader = '#version 300 es' + "\n" +
-    'in vec4 a_position;' + "\n" +
-    'in vec3 a_norm;' + "\n" +
-    'in vec2 a_uv;' + "\n" +
-    'uniform mat4 uPMatrix;' + "\n" +
-    'uniform mat4 uMVMatrix;' + "\n" +
-    'uniform mat4 uCameraMatrix;' + "\n" +
-    'uniform float uPositonX;' + "\n" +
-    'uniform float uPositonY;' + "\n" +
-    'out highp vec2 vUV;' + "\n" +
-    'const float size = 1.0/16.0;' + "\n" +
-    'void main(void){' + "\n" +
-    '	int f = int(a_position.w);' + "\n" +
-    'float u = uPositonX * size + a_uv.x * size;' + "\n" +
-    'float v = uPositonY* size + a_uv.y * size;' + "\n" +
-    'vUV = vec2(u,v);' + "\n" +
-    'gl_Position = uPMatrix * uCameraMatrix * uMVMatrix * vec4(a_position.xyz, 1.0); ' + "\n" +
-    '}';
-var gFragment_shader = '#version 300 es ' + "\n" +
-    'precision mediump float; ' + "\n" +
-    'uniform sampler2D uAltas; ' + "\n" +
-    'in highp vec2 vUV; ' + "\n" +
-    'out vec4 outColor; ' + "\n" +
-    'void main(void){ outColor = texture(uAltas,vUV); } ';
-var uPositonX = 0;
-var uPositonY = 0;
-var temp = 15;
-var NewLine = 0;
+var _a = gShaderFunction(), gShader = _a.gShader, gVertex_shader = _a.gVertex_shader, gFragment_shader = _a.gFragment_shader;
 var moveBot = [];
-function main() {
+var _b = BotHelperNumbers(), uPositonX = _b.uPositonX, uPositonY = _b.uPositonY, temp = _b.temp, NewLine = _b.NewLine;
+function gShaderFunction() {
+    var gShader = null;
+    var gVertex_shader = "";
+    var gFragment_shader = "";
+    return { gShader: gShader, gVertex_shader: gVertex_shader, gFragment_shader: gFragment_shader };
+}
+function BotHelperNumbers() {
+    var uPositonX = 0;
+    var uPositonY = 0;
+    var temp = 15;
+    var NewLine = 0;
+    return { uPositonX: uPositonX, uPositonY: uPositonY, temp: temp, NewLine: NewLine };
+}
+//#region init webgl
+function main(VS, FS) {
+    gVertex_shader = VS;
+    gFragment_shader = FS;
     // init webgl2
     gl = GLInstance("webglCanvas").fFitScreen(0.95, 0.9).fClear();
     gCamera = new C_Camera(gl);
@@ -52,22 +39,32 @@ function main() {
     gRLoop = new C_RenderLoop(onRender, 30);
     C_Resources.setup(gl, onReady).loadTexture("atlas", gInputManager.atlasLink).start();
 }
+//#endregion
+//#region Load Objects
 function onReady() {
-    gShader = new C_ShaderBuilder(gl, gVertex_shader, gFragment_shader)
-        .prepareUniforms("uPMatrix", "mat4", "uMVMatrix", "mat4", "uCameraMatrix", "mat4", "uFaces", "2fv", "uPositonX", "fv", "uPositonY", "fv")
-        .prepareTextures("uAltas", "atlas")
-        .setUniforms("uPMatrix", gCamera.projectionMatrix);
-    var cubemesh = Primatives.Cube.createMesh(gl, "Cube", 1, 1, 1, 0, 0, 0, false);
-    gCamera.transform.position.set(7.5, -7.5, 14.7);
-    for (var i = 0; i < 256; i++) {
-        var model = new C_Modal(cubemesh).setPosition((i % 16), 0.0, -Math.floor(i / 16));
-        gCubes.push(model);
-        moveBot.push(new C_MoveBot(model.transform, model.transform.position));
-        moveBot[i].SetPosition(new C_Vector3(gInputManager.startPos.x, gInputManager.startPos.y, gInputManager.startPos.z));
-        moveBot[i].SetSpeed(1.0);
-    }
+    initShader();
+    initCubs();
     gRLoop.start();
+    function initCubs() {
+        var cubemesh = Primatives.Cube.createMesh(gl, "Cube", 1, 1, 1, 0, 0, 0, false);
+        gCamera.transform.position.set(7.5, -7.5, 14.7);
+        for (var i = 0; i < 64; i++) {
+            var model = new C_Modal(cubemesh).setPosition((i % 8), 0.0, -Math.floor(i / 8));
+            gCubes.push(model);
+            moveBot.push(new C_MoveBot(model.transform, model.transform.position));
+            moveBot[i].SetPosition(new C_Vector3(gInputManager.startPos.x, gInputManager.startPos.y, gInputManager.startPos.z));
+            moveBot[i].SetSpeed(1.0);
+        }
+    }
+    function initShader() {
+        gShader = new C_ShaderBuilder(gl, gVertex_shader, gFragment_shader)
+            .prepareUniforms("uPMatrix", "mat4", "uMVMatrix", "mat4", "uCameraMatrix", "mat4", "uFaces", "2fv", "uPositonX", "fv", "uPositonY", "fv")
+            .prepareTextures("uAltas", "atlas")
+            .setUniforms("uPMatrix", gCamera.projectionMatrix);
+    }
 }
+//#endregion
+//#region Render Loop 
 function onRender(dt) {
     gl.fClear();
     gCamera.updateViewMatrix();
@@ -86,6 +83,7 @@ function onRender(dt) {
         }
     }
 }
+//#endregion
 function SetPositioninTexture(i) {
     if (i >= temp) {
         temp += 16;
@@ -96,14 +94,13 @@ function SetPositioninTexture(i) {
         uPositonX++;
     }
 }
+//#region Shutdown
 function Shutdown() {
     gl = null;
     gModal = null;
     gCamera.rest();
     gCameraCtrl.rest();
     gShader = null;
-    gGridShader = null;
-    gGridModal = null;
     RLoop = null;
     gRLoop.rest();
     C_Resources.rest();
@@ -117,6 +114,7 @@ function Shutdown() {
     //  gVertex_shader = null;
     //  gFragment_shader = null;
 }
+//#endregion
 function NewStart() {
     Shutdown();
     gInputManager.update();
--- js/Main.ts
+++ js/Main.ts
@@ -1,59 +1,41 @@
 window.addEventListener("load",function(){
-    main();
+	G_LoadShader();
 });
 var gl:any = null;
 var gModal:any = null;
 var gCamera:C_Camera;
 var gCameraCtrl:C_CameraController;
-var gShader:any = null;
-var gGridShader:any = null;
-var gGridModal:any = null;
 var RLoop:any = null;
 var gRLoop:C_RenderLoop;
 var Resources:any = null;
 var gCubes:C_Modal[] = [];
 var gInputManager:C_InputManager = new C_InputManager();
-var gVertex_shader:any =  '#version 300 es' + "\n"+
-'in vec4 a_position;' + "\n"+
-'in vec3 a_norm;' + "\n"+
-'in vec2 a_uv;' + "\n"+
-
-'uniform mat4 uPMatrix;' + "\n"+
-'uniform mat4 uMVMatrix;' + "\n"+
-'uniform mat4 uCameraMatrix;' + "\n"+
-'uniform float uPositonX;' + "\n"+
-'uniform float uPositonY;' + "\n"+
-'out highp vec2 vUV;' + "\n"+
-
-'const float size = 1.0/16.0;' + "\n"+
-
-'void main(void){' + "\n"+
-'	int f = int(a_position.w);' + "\n"+
-	'float u = uPositonX * size + a_uv.x * size;' + "\n"+
-	'float v = uPositonY* size + a_uv.y * size;' + "\n"+
-	'vUV = vec2(u,v);' + "\n"+
-
-	'gl_Position = uPMatrix * uCameraMatrix * uMVMatrix * vec4(a_position.xyz, 1.0); ' + "\n"+
-'}';
-var gFragment_shader:any = 
-		'#version 300 es ' + "\n"+
-		'precision mediump float; ' + "\n"+
-		'uniform sampler2D uAltas; ' + "\n"+
-		'in highp vec2 vUV; ' + "\n"+
-		'out vec4 outColor; ' + "\n"+
-
-		'void main(void){ outColor = texture(uAltas,vUV); } ' ;
-var uPositonX:number = 0;
-var uPositonY:number = 0;
-var temp:number = 15;
-var NewLine:number = 0;
+var { gShader, gVertex_shader, gFragment_shader }: { gShader: any; gVertex_shader: any; gFragment_shader: any; } = gShaderFunction();
+		
 var moveBot:C_MoveBot[] = [];
+		
+var { uPositonX, uPositonY, temp, NewLine }: { uPositonX: number; uPositonY: number; temp: number; NewLine: number; } = BotHelperNumbers();
 
+function gShaderFunction() {
+	var gShader: any = null;
+	var gVertex_shader: any = "";
+	var gFragment_shader: any = "";
+	return { gShader, gVertex_shader, gFragment_shader };
+}
 
-function main():void
+function BotHelperNumbers() {
+	var uPositonX: number = 0;
+	var uPositonY: number = 0;
+	var temp: number = 15;
+	var NewLine: number = 0;
+	return { uPositonX, uPositonY, temp, NewLine };
+}
+
+//#region init webgl
+function main(VS:string,FS:string):void
 {
-
-
+	gVertex_shader = VS;
+	gFragment_shader = FS;
 	// init webgl2
 	gl = GLInstance("webglCanvas").fFitScreen(0.95,0.9).fClear();
 	gCamera = new C_Camera(gl);
@@ -62,29 +44,38 @@ function main():void
 	gRLoop = new C_RenderLoop(onRender,30);
 	C_Resources.setup(gl,onReady).loadTexture("atlas",gInputManager.atlasLink).start();
 }
+//#endregion
 
+//#region Load Objects
 function onReady():void{
-	gShader = new C_ShaderBuilder(gl,gVertex_shader,gFragment_shader)
-	.prepareUniforms("uPMatrix","mat4"
-	,"uMVMatrix","mat4"
-	,"uCameraMatrix","mat4"
-	,"uFaces","2fv",
-	"uPositonX","fv"
-	,"uPositonY","fv")
-	.prepareTextures("uAltas","atlas")
-	.setUniforms("uPMatrix",gCamera.projectionMatrix);
-	var cubemesh:any = Primatives.Cube.createMesh(gl,"Cube",1,1,1,0,0,0,false);
-	gCamera.transform.position.set(7.5,-7.5,14.7);
-	for(var i=0; i < 256; i++){
-			var model:C_Modal = new C_Modal(cubemesh).setPosition( (i%16 ) , 0.0 , -Math.floor(i/16) );
+	initShader();
+	initCubs();
+	gRLoop.start();	
+
+	function initCubs() {
+		var cubemesh: any = Primatives.Cube.createMesh(gl, "Cube", 1, 1, 1, 0, 0, 0, false);
+		gCamera.transform.position.set(7.5, -7.5, 14.7);
+		for (var i = 0; i < 64; i++) {
+			var model: C_Modal = new C_Modal(cubemesh).setPosition((i % 8), 0.0, -Math.floor(i / 8));
 			gCubes.push(model);
-			moveBot.push( new C_MoveBot(model.transform,model.transform.position));
-			moveBot[i].SetPosition(new C_Vector3(gInputManager.startPos.x,gInputManager.startPos.y,gInputManager.startPos.z));
+			moveBot.push(new C_MoveBot(model.transform, model.transform.position));
+			moveBot[i].SetPosition(new C_Vector3(gInputManager.startPos.x, gInputManager.startPos.y, gInputManager.startPos.z));
 			moveBot[i].SetSpeed(1.0);
 		}
-	gRLoop.start();	
 	}
 
+	
+	 function initShader() {
+		gShader = new C_ShaderBuilder(gl, gVertex_shader, gFragment_shader)
+			.prepareUniforms("uPMatrix", "mat4", "uMVMatrix", "mat4", "uCameraMatrix", "mat4", "uFaces", "2fv", "uPositonX", "fv", "uPositonY", "fv")
+			.prepareTextures("uAltas", "atlas")
+			.setUniforms("uPMatrix", gCamera.projectionMatrix);
+	}
+
+}
+//#endregion
+
+//#region Render Loop 
 function onRender(dt:number):void{
 		gl.fClear();
 		gCamera.updateViewMatrix();
@@ -105,7 +96,7 @@ function onRender(dt:number):void{
 			}
 		}
 }
-
+//#endregion
 
 function SetPositioninTexture(i:number):void{
 	if(i >= temp)
@@ -119,6 +110,7 @@ function SetPositioninTexture(i:number):void{
 	}
 }
 
+//#region Shutdown
 function Shutdown()
 {
 	 gl = null;
@@ -126,8 +118,6 @@ function Shutdown()
  gCamera.rest();
  gCameraCtrl.rest();
  gShader = null;
- gGridShader = null;
- gGridModal = null;
  RLoop = null;
  gRLoop.rest();
  C_Resources.rest();
@@ -142,6 +132,7 @@ function Shutdown()
  //  gVertex_shader = null;
  //  gFragment_shader = null;
 }
+//#endregion
 
 function NewStart()
 {
